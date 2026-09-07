78a4d455d6d79bb0d926b46a447120e2f36cc450
Fabien Heilles <phabi1@hotmail.fr>
2026-09-07 08:35:23 +0200

    feat: add file-preview component

---
+++ src/app/components/ui/file-preview/file-preview.ts
@@ -0,0 +1,38 @@
+import { Component, computed, input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-ui-file-preview',
+  imports: [MatIconModule],
+  templateUrl: './file-preview.html',
+  styleUrl: './file-preview.css',
+})
+export class FilePreview {
+  file = input.required<string>();
+
+  filetype = computed(() => {
+    const file = this.file();
+    const extension = file.substring(file.lastIndexOf('.'));
+    if (this.isImage(extension)) {
+      return 'image';
+    } else if (this.isPdf(extension)) {
+      return 'pdf';
+    }
+    return 'file';
+  });
+
+  filename = computed(() => {
+    const file = this.file();
+    return file.substring(file.lastIndexOf('/'));
+  });
+
+  url = computed(() => this.file());
+
+  private isImage(extension: string): boolean {
+    return ['jpg', 'jpeg,', 'gif', 'png', 'avif', 'heic', 'webp'].includes(extension);
+  }
+
+  private isPdf(extension: string): boolean {
+    return extension === 'pdf';
+  }
+}
