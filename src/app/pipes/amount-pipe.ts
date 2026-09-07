1dd0b65875938283a59a004e36b8345d331235a2
Fabien Heilles <phabi1@hotmail.fr>
2026-09-07 10:23:59 +0200

    fix: Display cents from amount

--- src/app/pipes/amount-pipe.ts
+++ src/app/pipes/amount-pipe.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AmountPipe implements PipeTransform {
   transform(value: number, ...args: unknown[]): unknown {
-    return value.toFixed(2) + ' €';
+    return (value / 100).toFixed(2) + ' €';
   }
 }
