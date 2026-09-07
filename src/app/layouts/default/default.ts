2d27a2a8a513637846c4214d97f585fbfb58598e
Fabien Heilles <phabi1@hotmail.fr>
2026-09-07 10:22:36 +0200

    feat: implements responsive sidebar

--- src/app/layouts/default/default.ts
+++ src/app/layouts/default/default.ts
@@ -1,13 +1,31 @@
-import { NgComponentOutlet } from '@angular/common';
-import { ChangeDetectionStrategy, Component, effect, inject, signal, Type } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { NgClass, NgComponentOutlet } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  inject,
+  signal,
+  Type,
+  viewChild,
+} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { distinctUntilChanged, filter, map, startWith } from 'rxjs';
+import { distinctUntilChanged, filter, map, shareReplay, startWith, withLatestFrom } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-layouts-default',
-  imports: [RouterOutlet, MatSidenavModule, NgComponentOutlet],
+  imports: [
+    RouterOutlet,
+    MatSidenavModule,
+    NgComponentOutlet,
+    NgClass,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './default.html',
   styleUrls: ['./default.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -15,15 +33,45 @@ import { distinctUntilChanged, filter, map, startWith } from 'rxjs';
 export class DefaultLayout {
   private router = inject(Router);
   private authService = inject(AuthService);
+  private breakpointObserver = inject(BreakpointObserver);
 
   private sidebars: Record<string, () => Promise<Type<any>>> = {
-    'default': () => import('../../components/layouts/sidebars/default/default').then(m => m.Default),
-    'membership-campaign': () => import('../../components/layouts/sidebars/campaign/campaign').then(m => m.Campaign)
-  }
+    default: () =>
+      import('../../components/layouts/sidebars/default/default').then((m) => m.Default),
+    'membership-campaign': () =>
+      import('../../components/layouts/sidebars/campaign/campaign').then((m) => m.Campaign),
+  };
+
+  sidenav = viewChild<MatSidenav>('sidenav');
 
   sidebar = signal<Type<any> | null>(null);
 
+  isHandset = signal<boolean>(false);
+
   constructor() {
+    effect(() => {
+      const subscription = this.breakpointObserver
+        .observe([Breakpoints.Handset, Breakpoints.TabletPortrait])
+        .pipe(
+          map((result) => result.matches),
+          shareReplay(),
+        )
+        .subscribe((isHandset) => {
+          this.isHandset.set(isHandset);
+        });
+      return () => subscription.unsubscribe();
+    });
+    effect(() => {
+      const subscription = this.router.events
+        .pipe(filter((event) => event instanceof NavigationEnd))
+        .subscribe(() => {
+          const sidenav = this.sidenav();
+          if (this.isHandset() && sidenav) {
+            sidenav.close();
+          }
+        });
+      return () => subscription.unsubscribe();
+    });
     effect(() => {
       const subciption = this.router.events
         .pipe(
