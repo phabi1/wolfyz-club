e62b625ed9f2cffd60b7e81b930c98c03aea7442
Fabien Heilles <phabi1@hotmail.fr>
2026-09-08 21:38:25 +0200

    feat: display new layout lesson completude

--- src/app/components/membership/dashboard/widgets/lesson-completude/lesson-completude.ts
+++ src/app/components/membership/dashboard/widgets/lesson-completude/lesson-completude.ts
@@ -1,7 +1,8 @@
-import { Component, effect, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal, computed } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { LessonService } from '../../../../../services/membership/lesson.service';
 import type { Lesson } from '../../../../../models/membership/lesson';
+import { formatLessonTitle } from '../../../../../utils/lesson';
 
 @Component({
   selector: 'app-lesson-completude',
@@ -13,11 +14,28 @@ export class LessonCompletude {
   private readonly lessonService: LessonService = inject(LessonService);
   lessons = signal<Lesson[]>([]);
 
+  sortedLessons = computed(() => {
+    return this.lessons()
+      .slice()
+      .sort((a, b) => {
+        const d1 = a.day === 0 ? 7 : a.day;
+        const d2 = b.day === 0 ? 7 : b.day;
+        if (d1 === d2) {
+          return a.lesson_start.getTime() - b.lesson_start.getTime();
+        }
+        return d1 - d2;
+      });
+  });
+
   constructor() {
     effect(() => {
-      this.lessonService.items(2).subscribe(response => {
+      this.lessonService.items(2).subscribe((response) => {
         this.lessons.set(response.items);
       });
     });
+  }
+
+  formatLessonTitle(lesson: Lesson): string {
+    return formatLessonTitle(lesson);
   }
 }
